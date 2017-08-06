b5666b26a4428b2e41cff5c42c1e7613b02a5105
Andrew Henrique <andrewhenriquef@gmail.com>
2017-08-06 00:24:42 -0300

    Fix active admin permit_params for schedule

--- app/admin/schedule.rb
+++ app/admin/schedule.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register Schedule do
-# See permitted parameters documentation:
+    permit_params :description, :start_at, :end_at
+    # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 # permit_params :list, :of, :attributes, :on, :model
